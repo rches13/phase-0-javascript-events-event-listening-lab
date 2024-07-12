04b570b4315f2abe24eb584c11f276438c601622
Rodgers Chesoni <rodgerschesoni@gmail.com>
2024-07-12 15:27:05 +0300

    add events

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+
 function addingEventListener() {
-}
+    // Get the button element by its ID
+    const button = document.getElementById('button');
+    
+    // Add an event listener to the button
+    button.addEventListener('click', function() {
+      alert('Button was clicked!');
+    });
+  }
+  
+  // Call the function to bind the event listener
+  addingEventListener();
+  
